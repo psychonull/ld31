50d4ea6f635ea5c3243c2106ced8c1b5ab718599
Pablo Novas <pjnovas@gmail.com>
2014-12-07 01:08:13 -0300

    added granma animation sprites

--- src/animations/GrandMa.js
+++ src/animations/GrandMa.js
@@ -2,18 +2,25 @@
 module.exports = {
 
   walkRight: new pac.Animation({
-    fps: 3,
-    frames: ['right1','right2','right3','right4','right5']
+    fps: 8,
+    frames: ['right1','right2','right3','right4',
+             'right5','right6','right7','right8']
   }),
 
   walkLeft: new pac.Animation({
-    fps: 3,
-    frames: ['left1','left2','left3','left4','left5']
+    fps: 8,
+    frames: ['left1','left2','left3','left4',
+             'left5','left6','left7','left8']
   }),
 
-  idle: new pac.Animation({
-    fps: 3,
-    frames: ['idle1','idle2','idle3','idle4','idle5']
+  idleLeft: new pac.Animation({
+    fps: 2,
+    frames: ['idle1','idle2','idle3']
+  }),
+
+  idleRight: new pac.Animation({
+    fps: 2,
+    frames: ['idle4','idle5','idle6']
   }),
 
 };

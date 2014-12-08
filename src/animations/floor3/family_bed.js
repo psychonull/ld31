afaba8540cf9f5df006854160b6f2d9041c0db03
Marcelo <marcelogvallejos@gmail.com>
2014-12-07 21:18:59 -0300

    Adding falily bed .json, js, floorObject

---
+++ src/animations/floor3/family_bed.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+
+  bedOn: new pac.Animation({
+    fps: 1,
+    frames: ['family_bed_on_1','family_bed_on_0','family_bed_on_1']
+  }),
+
+  bedOff: new pac.Animation({
+    fps: 1,
+    frames: ['family_bed']
+  }),
+
+  bedHover: new pac.Animation({
+    fps: 1,
+    frames: ['family_bed_hover']
+  })
+
+};
